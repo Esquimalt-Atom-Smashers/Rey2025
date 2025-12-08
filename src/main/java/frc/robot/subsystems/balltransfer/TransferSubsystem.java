be7d118a940c4945d0dbd83fd0fe4ee5167bffb9
21stcenturymacgyver <21stcenturymacgyver@gmail.com>
2025-12-08 14:13:52 -0800

    Initial Commit Code Layout built

---
+++ src/main/java/frc/robot/subsystems/balltransfer/TransferSubsystem.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.balltransfer;
+
+public class TransferSubsystem {
+
+}
---
+++ src/main/java/frc/robot/subsystems/ledlights/BlinkinSubsystem.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.ledlights;
+
+public class BlinkinSubsystem {
+    
+}
