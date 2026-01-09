272e4969af82d213f0b8a6de00a1514ec5a114f3
mydimons <mydimons.kb@gmail.com>
2026-01-09 13:57:56 -0800

    Create motor id constants files

---
+++ src/main/java/frc/robot/subsystems/PhoenixIDConstants.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+public final class PhoenixIDConstants {
+    // Left Drive
+    public static final int LEFT_DRIVE_VICTOR_1 = 1;
+    public static final int LEFT_DRIVE_TALON = 1;
+    public static final int LEFT_DRIVE_VICTOR_2 = 3;
+
+    // Right Drive
+    public static final int RIGHT_DRIVE_VICTOR_1 = 4;
+    public static final int RIGHT_DRIVE_TALON = 5;
+    public static final int RIGHT_DRIVE_VICTOR_2 = 6;
+
+    // Lift/Hang
+    public static  final int LIFT_HANG = 9;
+
+    // Intake
+    public static final int INTAKE = 10;
+    public static final int PNEUMATIC_CONTROL_MODULE = 13;
+
+    // Ball Transferer
+    public static final int BALL_TRANSFER = 11;
+
+    // Shooter
+    public static final int FLYWHEEL = 7;
+    public static final int SHOOTER_FEEDER = 8;
+    public static final int HOOD_LINEAR_ACTUATOR = 12;
+    public static final int GRENADE = 15;
+}
